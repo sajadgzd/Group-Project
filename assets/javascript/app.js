a302626d350ae78148eadc9925123471877e1060
sajad1993gh <sajad1993gh@gmail.com>
2019-06-15 21:15:09 -0400

    getting locations

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,8 @@
 // code to retrieve the location of the user. PUT ALL YOUR CODES UNDER LINE 67
 var latitude;
 var longitude;
+var businessId = [];
+var reviewURL;
 
 function ipLookUp() {
     $.ajax('http://ip-api.com/json')
@@ -62,10 +64,11 @@ if ("geolocation" in navigator) {
 $(document).ready(function() {
 
     // https: //api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972
-    var restaurantURLBase = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/";
+    var restaurantURLBase = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/";
+
     $("#run-search").on("click", function() {
         var term = "chinese";
-        var restaurantURL = restaurantURLBase + "businesses/search?term=" + term + "&latitude=" + latitude + "&longitude=" + longitude;
+        var restaurantURL = restaurantURLBase + "search?term=" + term + "&latitude=" + latitude + "&longitude=" + longitude;
         $.ajax({
             url: restaurantURL,
             method: "GET",
@@ -73,9 +76,25 @@ $(document).ready(function() {
                 xhr.setRequestHeader("Authorization", "Bearer 3wWOAvaGNXrcyeiEyHu-LozubQFqCpPz8_zacZInc3dFC9Dqgy8yuMqUFwRoj9dnb1xhuNPqMP2tY1NTGiq60ACjN-cRCMfIViTZJYkuWvej58Glaemaz2Pv_1AEXXYx");
             },
         }).then(function(response) {
-            console.log(response);
-            console.log(restaurantURL);
-            for (let i = 0; i < 10; i++) {
+            // console.log(response);
+
+            for (let i = 0; i < 5; i++) {
+
+                // Getting Reviews
+                // console.log("ID# : ", response.businesses[i].id);
+                reviewURL = restaurantURLBase + response.businesses[i].id + "/reviews";
+                // console.log(reviewURL);
+                $.ajax({
+                    url: reviewURL,
+                    method: "GET",
+                    beforeSend: function(xhr) {
+                        xhr.setRequestHeader("Authorization", "Bearer 3wWOAvaGNXrcyeiEyHu-LozubQFqCpPz8_zacZInc3dFC9Dqgy8yuMqUFwRoj9dnb1xhuNPqMP2tY1NTGiq60ACjN-cRCMfIViTZJYkuWvej58Glaemaz2Pv_1AEXXYx");
+                    },
+                }).then(function(response) {
+                    console.log(response.reviews[0].text);
+                });
+
+                // Getting Business Info
                 console.log("NAME: ", response.businesses[i].name);
                 console.log("PHONE NUMBER: ", response.businesses[i].display_phone);
                 console.log("PRICING RATE: ", response.businesses[i].price);
@@ -91,21 +110,11 @@ $(document).ready(function() {
                 }
             }
 
-        })
+        });
+
+
 
     });
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
